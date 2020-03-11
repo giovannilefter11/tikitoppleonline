16b4ab0fb617d79e55c29cf276f65d11719d9a59
giovannilefter11 <61161426+giovannilefter11@users.noreply.github.com>
2020-03-11 02:45:16 +0100

    modifiche varie socket

--- client/src/components/TikiTopple.vue
+++ client/src/components/TikiTopple.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <canvas
-      ref="game"
-      width="500"
-      height="500"
-      style="border: 1pt solid black"
-    >
-
+  <div id="container">
+    <canvas ref="game">
     </canvas>
+    <div class="events-log">
+      <ul>
+        <li v-for="message in messages" :key="message">
+          {{message}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -18,30 +19,65 @@
     data() {
       return {
         socket: {},
-        context: {},
         player: {},
-        isViewMode: false
+        color: {},
+        score: {},
+        goal: {},
+        canPlay: {},
+        messages: []
       }
     },
     created() {
       this.socket = io("http://localhost:3000");
     },
     mounted() {
-      this.socket.on("connection-enstablished", isViewMode => {
-        this.isViewMode = isViewMode;
-      });
-
-      console.log(this.isViewMode);
-      if (!this.isViewMode) {
-        console.log("chiediamo un colore");
+      this.printMessage("Entrando nella stanza...");
+      this.checkCanPlay();
+      if (this.canPlay) {
+        // chiediamo un colore per questo giocatore al socket
         this.socket.emit("ask-color");
-        this.socket.on("emit-color", data => {
-          console.log("colore di appartenenza " + data);
+        // e ci salviamo il colore che ci viene restituito
+        this.socket.on("emit-color", color => {
+          this.color = color;
         });
+        this.socket.on("send-msg", msg => {
+          this.printMessage(msg);
+        });
+      }
+    },
+    methods: {
+      checkCanPlay: function () {
+        this.socket.on("connection-enstablished", canPlay => {
+          if (!canPlay) {
+            this.printMessage("Non puoi giocare");
+          }
+          this.canPlay = canPlay;
+        });
+      },
+      printMessage: function (msg) {
+        this.messages.push(msg);
       }
     }
   }
 </script>
 
 <style scoped>
+  #container{
+    display: flex;
+  }
+
+  canvas {
+    height:500px;
+    border: 1pt solid black;
+    flex-grow: 1;
+    flex-basis: 0;
+  }
+
+  .events-log {
+    background-color: lightgray;
+    height: 500px;
+    flex-grow: 1;
+    flex-basis: 0;
+    overflow-y: scroll;
+  }
 </style>
--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-// npm start
-// npm run serve
+// npm start --> fa partire il server
+// npm run serve --> client
 
+// istanze per connessione socket
 const express = require("Express")();
 const http = require("http").Server(express);
 const socketio = require("socket.io")(http);
 
+// costanti dell'app
 const steps = 33; // numero di tasselli nel gioco
 const maxPlayersNumber = 4; // numero di giocatori
 const playerColors = ["red", "blue", "yellow", "green"]; // colori standard dei giocatori
-let activePlayersColors = []; // array coi colori dei giocatori già in gioco
 const blocks = [ // blocchetti che si muoveranno
     { name: "HUHU", color: "yellow" },
     { name: "WIKIKI", color: "lightblue" },
@@ -20,39 +21,78 @@ const blocks = [ // blocchetti che si muoveranno
     { name: "EEPO", color: "brown" },
     { name: "HOOKIPA", color: "grey" },
 ];
-let activePlayersClients = [];
 
+let activePlayersClients = []; // array coi giocatori attivi
+let gameStarted = false;
+
+// ci mettiamo in ascolto sulla porta 3000
 http.listen(port = 3000, () => {
-    console.log("listen on port " + port);
+    console.log("listening on port " + port);
 });
 
+// quando un utente si connetterà atterrerà in questo evento
 socketio.on("connection", socket => {
     console.log("connessione effettuata");
 
-    function isViewMode() {
-        return activePlayersClients.length >= maxPlayersNumber;
+    // evento di connessione stabilita, comunichiamo all'utente se può giocare o meno
+    socket.emit("connection-enstablished", canPlay());
+
+    console.log(canPlay() ? "può giocare" : "non può giocare");
+
+    // se questo utente può giocare andiamo avanti, altrimenti è inutile continuare
+    if (canPlay()){
+        // quando un giocatore si connetterà dovremmo estrarre un colore a caso e assegnarglielo da quelli rimanenti
+        socket.on("ask-color", () => {
+            // estraiamo un colore a caso per l'utente appena collegato
+            let color = generateValidRandomColor();
+            activePlayersClients.push({socket: socket, color: color});
+            socket.emit("emit-color", color);
+            console.log("giocatori attivi: " + activePlayersClients.length);
+            socket.emit('send-msg', 'Sei il ' + color);
+            socket.broadcast.emit('send-msg', 'Si è connesso il giocatore ' + color);
+        });
     }
 
-    socket.emit("connection-enstablished", isViewMode());
-
-    // quando un giocatore si connetterà dovremmo estrarre un colore a caso e assegnarglielo da quelli rimanenti
-    socket.on("ask-color", () => {
-        console.log("è stato chiesto un colore");
-        let color = playerColors[Math.floor(Math.random() * playerColors.length)];
-        if (!activePlayersColors.includes(color) && !isViewMode()){
-            activePlayersClients.push({socket: socket, color: color});
-            activePlayersColors.push(color);
-            socket.emit("emit-color", color);
-            console.log("emesso colore " + color);
-            activePlayersClients.length;
-            console.log("giocatori attivi: "+activePlayersClients.length);
-        }
-    })
-
-    socket.on("disconnect", () => {
-        console.log("un giocatore è uscito");
-        let i = activePlayersClients.indexOf(socket);
-        activePlayersClients.splice(i, 1);
-        console.log("giocatori rimasti: " + activePlayersClients.length);
-    });
+    // socket.on("disconnect", () => {
+    //     console.log("un giocatore è uscito");
+    //     let i = activePlayersClients.indexOf(socket);
+    //     activePlayersClients.splice(i, 1);
+    //     console.log("giocatori rimasti: " + activePlayersClients.length);
+    // });
 });
+
+/**
+ * controlliamo che la stanza non sia già piena
+ * @returns {boolean}
+ */
+function canPlay() {
+    return activePlayersClients.length < maxPlayersNumber;
+}
+
+/**
+ * troviamo random un colore che non sia già stato preso dagli utenti attivi
+ * @returns {string}
+ */
+function generateValidRandomColor() {
+    let color;
+    // se tra gli utenti attivi c'è già il colore estratto dobbiamo estrarne un altro
+    do {
+        color = playerColors[Math.floor(Math.random() * playerColors.length)];
+    } while (existsActivePlayersWithColor(color));
+    return color;
+}
+
+/**
+ * controlliamo se nell'oggetto dei giocatori attivi è già stato assegnato questo colore
+ * @param color
+ * @returns {boolean}
+ */
+function existsActivePlayersWithColor(color){
+    let ret = false;
+    activePlayersClients.map(p =>{
+        if(p.color === color) {
+            ret = true;
+        }
+    });
+    return ret;
+}
